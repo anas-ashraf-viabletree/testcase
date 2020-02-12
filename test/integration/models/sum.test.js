191dc0f1d37844f1e916a8e3af51c84f7f4b1064
Tasneem Viabletree <tasneem.faizyab@viabletree.com>
2020-02-12 17:22:17 +0500

    asdfasdf

--- test/integration/models/sum.test.js
+++ test/integration/models/sum.test.js
@@ -9,7 +9,7 @@ describe('User (model)', () => {
         supertest(sails.hooks.http.app)
             .post('/api/v1/math/sum')
             .send({ num1: 1, num2: 2 })
-            .expect(400)
+            .expect(200)
             .end(done);
 
 
